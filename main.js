b2d934d0e322cc79fd70e21b53ded9e177f99ad8
Ryan Korinek <korinekonie@gmail.com>
2023-02-22 19:31:52 -0800

    Change grid size with slider
    Can now change the grid size dynamically using the slider.

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 'use-strict';
 
 const gridContainer = document.querySelector('div.grid-container');
-const slider = document.querySelector('.slider');
+const slider = document.getElementById('myRange');
 let gridSquares;
 
-function createSquares(numberOfSquares) {
+function initialSettings() {
+  createGrid(100);
+  slider.addEventListener('input', changeGridSize);
+  gridSquares.forEach(square => square.addEventListener('mouseover', addHover));
+}
+
+function createGrid(numberOfSquares) {
   let count = numberOfSquares;
   while (count > 0) {
     gridContainer.appendChild(document.createElement('div'));
@@ -17,6 +23,12 @@ function addHover(event) {
   event.target.classList.add('grid-square-hover');
 }
 
-createSquares(36);
+function changeGridSize(event) {
+  let sliderValue = Number(slider.value);
+  gridContainer.replaceChildren();
+  createGrid(sliderValue * sliderValue);
+  gridContainer.style.gridTemplateColumns = `repeat(${slider.value}, auto)`;
+  gridSquares.forEach(square => square.addEventListener('mouseover', addHover));
+}
 
-gridSquares.forEach(square => square.addEventListener('mouseover', addHover));
+initialSettings();
