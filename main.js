c1e2ee19e665805c7495a8f1eafa0d4f7f027381
Ryan Korinek <korinekonie@gmail.com>
2023-02-22 17:18:35 -0800

    Display squares and slider
    Generated a 4x4 square grid and displayed a slider with no functionality.

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 'use-strict';
+
+const gridContainer = document.querySelector('div.grid-container');
+const slider = document.querySelector('.slider');
+let gridSquares;
+
+function createSquares(numberOfSquares) {
+  let count = numberOfSquares;
+  while (count > 0) {
+    gridContainer.appendChild(document.createElement('div'));
+    count--;
+  }
+  gridSquares = document.querySelectorAll('.grid-container div');
+}
+
+function addHover(event) {
+  event.target.classList.add('grid-square-hover');
+}
+
+createSquares(16);
+
+gridSquares.forEach(square => square.addEventListener('mouseover', addHover));
