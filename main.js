0e363e2466b6e4321ab582988cb0d6da806458d1
Ryan Korinek <korinekonie@gmail.com>
2023-02-22 20:05:56 -0800

    Clear button correctly resets the app
    Clear button functionality is working. It resets the state of the app.

--- main.js
+++ main.js
@@ -2,11 +2,24 @@
 
 const gridContainer = document.querySelector('div.grid-container');
 const slider = document.getElementById('myRange');
+const clearBtn = document.querySelector('.clear-btn');
 let gridSquares;
+let sliderValue = slider.value;
 
 function initialSettings() {
-  createGrid(100);
+  createGrid(sliderValue * sliderValue);
+  gridContainer.style.gridTemplateColumns = `repeat(${20}, auto)`;
   slider.addEventListener('input', changeGridSize);
+  gridSquares.forEach(square => square.addEventListener('mouseover', addHover));
+  clearBtn.addEventListener('click', resetSettings);
+}
+
+function resetSettings(event) {
+  gridContainer.replaceChildren();
+  slider.value = 20;
+  sliderValue = slider.value;
+  createGrid(sliderValue * sliderValue);
+  gridContainer.style.gridTemplateColumns = `repeat(${sliderValue}, auto)`;
   gridSquares.forEach(square => square.addEventListener('mouseover', addHover));
 }
 
@@ -24,7 +37,7 @@ function addHover(event) {
 }
 
 function changeGridSize(event) {
-  let sliderValue = Number(slider.value);
+  sliderValue = Number(slider.value);
   gridContainer.replaceChildren();
   createGrid(sliderValue * sliderValue);
   gridContainer.style.gridTemplateColumns = `repeat(${slider.value}, auto)`;
